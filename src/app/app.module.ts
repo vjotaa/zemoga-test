4c9523ec6dd283d386cfb3f943e8d4ba88d69e75
Victor <v.manrique@druo.com>
2020-07-10 18:05:59 -0500

    feature: UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { VoteCardComponent } from './components/vote-card/vote-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CardComponent,
+    VoteCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/vote-card/vote-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vote-card',
+  templateUrl: './vote-card.component.html',
+  styleUrls: ['./vote-card.component.scss']
+})
+export class VoteCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
